13f27c4987ae887b81916f3a5fd6a2bccf248788
ibrahimkanan <ibrahimkanan20@gmail.com>
2026-07-17 21:00:43 +0300

    feat : setup the Database with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5003;
 
+app.use("/api/auth", authRoutes);
 
-
-app.listen(3000, () =>{
-    console.log("server is running on prot 3000")
-})
+app.listen(PORT, () => {
+    connectDB();
+    console.log("server is running on prot ", PORT);
+});
