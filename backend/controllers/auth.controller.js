512c4f137d417e50c5a685ecdf38514b72408fa8
ibrahimkanan <ibrahimkanan20@gmail.com>
2026-07-20 14:22:11 +0300

    feat : setup login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -92,8 +92,44 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    res.send("Login route");
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Invalid credentials" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Invalid credentials" });
+        }
+
+        genereateTokenAndSetCookie(res, user._id);
+
+        user.lastLogin = Date.now();
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: "undefined",
+            },
+        });
+    } catch (error) {
+        console.log("Login failed", error.message);
+        res.status(400).json({ success: false, message: error.message });
+    }
 };
+
 export const logout = async (req, res) => {
     try {
         res.clearCookie("jwt");
